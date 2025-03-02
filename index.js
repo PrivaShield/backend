2aed2da9d1487a0a92cfdaaa293847ddd78ecb13
dpgnqkqh14 <hhjin1403@gmail.com>
2025-03-03 02:25:59 +0900

    add auth feature

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+// const authMiddleware = require('./middleware/authMiddleware');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// 미들웨어 설정
+// CORS 설정
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }));
 
+app.use(express.json());
 app.use(bodyParser.json());
+
+// 라우터 설정
+app.use('/api/auth', authRoutes);
 
 // 테스트용 API
 app.get("/", (req, res) => {
